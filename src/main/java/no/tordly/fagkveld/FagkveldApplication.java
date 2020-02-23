d3342ed1ec493bd26a9593ae1ac294dce44a218e
robin tordly <robintordly@robins-MacBook-Pro.local>
2020-02-23 11:47:15 +0100

    init

---
+++ src/main/java/no/tordly/fagkveld/FagkveldApplication.java
@@ -0,0 +1,13 @@
+package no.tordly.fagkveld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FagkveldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FagkveldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/tordly/fagkveld/FagkveldApplicationTests.java
@@ -0,0 +1,13 @@
+package no.tordly.fagkveld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FagkveldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
